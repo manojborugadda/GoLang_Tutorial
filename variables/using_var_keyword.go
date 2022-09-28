66132ab8f53da40f64ac0c168e4d71bec353e719
Borugadda Manoj <borugadda.manoj@gmail.com>
2022-09-28 23:54:06 +0530

    Create using_var_keyword.go

---
+++ variables/using_var_keyword.go
@@ -0,0 +1,25 @@
+//Variable with zero value
+
+package main
+import "fmt"
+func main() {
+  
+  var a int
+  var b string 
+  var c float64
+  var d bool
+  
+    fmt.Printf("%v \n", a)
+    fmt.Printf("%v \n", b)
+    fmt.Printf("%v \n", c)
+    fmt.Printf("%v \n", d)
+}
+
+
+/*
+output will be :
+0
+      -------------basically it is empty space 
+0
+false
+*/
